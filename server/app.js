36f50134e29c1209e77f05e0a3c67e80687c4f49
Ahmed Abu Sharar <absam9945@gmail.com>
2022-09-28 16:53:21 +0300

    heroke close #19

--- server/app.js
+++ server/app.js
@@ -13,6 +13,12 @@ app.use(express.urlencoded({ extended: false }));
 app.disable('x-powered-by');
 
 app.set('port', process.env.PORT || 4000);
-app.use(express.static(join(__dirname, '..', 'client','build')));
-app.use(router);
+app.use(router); 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(join(__dirname, '..', 'client','build'))); 
+    app.get('*', (req, res)=> {
+        res.sendFile(join(__dirname, '..', 'client','build', 'index.html'))
+    })
+}
 module.exports = { app };
